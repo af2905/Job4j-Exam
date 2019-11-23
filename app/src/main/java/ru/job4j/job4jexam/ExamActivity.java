8d33c7633cd50684c8047e39d188e3fdb272a3ef
af2905 <af2905g@gmail.com>
2019-11-23 16:58:21 +0300

    Creating a project and launching through the Genymotion emulator [# 61514]

---
+++ app/src/main/java/ru/job4j/job4jexam/ExamActivity.java
@@ -0,0 +1,14 @@
+package ru.job4j.job4jexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
