090b6ae380e11a2859856ad930c02988ab7c97c3
af2905 <af2905g@gmail.com>
2019-11-24 13:07:17 +0300

    Activity. Life cycle. [# 61528]

--- app/src/main/java/ru/job4j/job4jexam/ExamActivity.java
+++ app/src/main/java/ru/job4j/job4jexam/ExamActivity.java
@@ -1,14 +1,60 @@
 package ru.job4j.job4jexam;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ExamActivity extends AppCompatActivity {
+    private static final String TAG = "ExamActivity";
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.custom_layout);
+        setContentView(R.layout.activity_exam);
+        if (!(savedInstanceState == null)) {
+            count = savedInstanceState.getInt("count");
+        }
+        Log.d(TAG, "onCreate");
+        Log.d(TAG, "count = "+ count);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ++count;
+        outState.putInt("count", count);
+        Log.d(TAG, "OnSaveInstanceState");
     }
 }
